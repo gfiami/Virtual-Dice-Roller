5dace0d54fcc8673b9954ce66ae4adeba8fe80a4
Gabriel Fiami <gabrielfiami.dev@gmail.com>
2023-03-11 15:22:19 -0300

    user can input dice number and sides to roll, and a message appears with the result

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
 const diceContainer = document.querySelector('.dicesInputContainer');
-const newDice = "Roll <input type='number' class='numberOfDice' value='1' min='1'> dice with <input type='number'class='numberOfSides' value='1' min='1'> sides <input type='button' onclick='addToCurrentDice()'class='addDice'value='+'> <br> <br>"
+const newDice = "Roll <input type='number' class='numberOfDice' value='1' min='1'> dice with <input type='number'class='numberOfSides' value='1' min='1'> sides <input type='button' onclick='addToCurrentDice()'class='addDice'value='Roll'> <br> <br>"
 diceContainer.innerHTML = newDice;
 
 let numbersToRoll = document.querySelector('.numberOfDice');
-let valueToRoll;
+let diceToRoll = 1
 numbersToRoll.addEventListener("input", (event)=>{
+    diceToRoll = numbersToRoll.value
     if(numbersToRoll.value.length > 4){
-        valueToRoll = numbersToRoll.value;
         numbersToRoll.value = ""
         for(i=0;i<4;i++){
-            numbersToRoll.value += valueToRoll[i];
+            numbersToRoll.value += diceToRoll[i];
         }
-        valueToRoll = numbersToRoll.value
+        diceToRoll = numbersToRoll.value
     }
     });
 
 let sidesOfDice = document.querySelector('.numberOfSides');
-let valueSides;
+let valueSides = 1
 sidesOfDice.addEventListener("input", (event) =>{
+    valueSides = sidesOfDice.value;
     if(sidesOfDice.value.length > 3){
-        valueSides = sidesOfDice.value;
         sidesOfDice.value = ""
         for(i=0;i<3;i++){
             sidesOfDice.value += valueSides[i];
@@ -28,12 +28,16 @@ sidesOfDice.addEventListener("input", (event) =>{
     }
 })
 
-
-
-
-
-
+const currentDice = document.querySelector('.currentDice');
 function addToCurrentDice(){
-    
+    currentDice.innerHTML = "";
+    let sum = 0;
+
+    for(i=0;i<diceToRoll;i++){
+        sum += Math.floor(Math.random() * valueSides) + 1;
+        console.log(sum);
+    }
+    currentDice.innerHTML = `Rolling ${diceToRoll} dice ${valueSides} sided... <br>`
+    currentDice.innerHTML += `Total: ${sum}`
 
 }
