d375a20875f9abf69d65d90157b8e440ecc83392
Gabriel Fiami <gabrielfiami.dev@gmail.com>
2023-03-11 20:09:25 -0300

    adjust modifier button to accept only numbers and set new layout

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 let numbersToRoll = document.querySelector('.numberOfDice');
 let diceToRoll = 1
+const modifierInput = document.querySelector('.numberOfModifier');
+let modifierValue = 0;
 numbersToRoll.addEventListener("input", (event)=>{
-    checkInputType(event.data);
+    console.log(event)
+    if(checkInputType(event.data) == false){
+        return false;
+    }
     diceToRoll = numbersToRoll.value
     if(numbersToRoll.value.length > 4){
         numbersToRoll.value = ""
@@ -15,13 +20,36 @@ numbersToRoll.addEventListener("keydown", (event)=>{
     //PREVENT CONTRL C CONTROL V
     if(event.key == 'Control' || event.key == 'C' || event.key == 'c' || event.key == 'V' || event.key == 'v'){
         setTimeout(() => {
-        numbersToRoll.value = ""
-
+        numbersToRoll.value = "1"
+        diceToRoll = numbersToRoll.value;
             }, 100);
     }})
 
 let valueSides = 6;
+//
+modifierInput.addEventListener("input", (event)=>{
+    if(checkModifierType(event.data) == false){
+        return false;
+    }
+    modifierValue = modifierInput.value
+    if(modifierInput.value.length > 3){
+        modifierInput.value = ""
+        for(i=0;i<3;i++){
+            modifierInput.value += modifierValue[i];
+        }
+        modifierValue = modifierInput.value
+    }
+    });
+    modifierInput.addEventListener("keydown", (event)=>{
+    //PREVENT CONTRL C CONTROL V
+    if(event.key == 'Control' || event.key == 'C' || event.key == 'c' || event.key == 'V' || event.key == 'v'){
+        setTimeout(() => {
+            modifierInput.value = "0"
+            modifierValue = modifierInput.value;
+            }, 100);
+    }})
 
+//
 /* USEFUL FOR CUSTOM INPUTS ONLY
 let sidesOfDice = document.querySelector('.numberOfSides');
 sidesOfDice.addEventListener("input", (event) =>{
@@ -40,7 +68,6 @@ sidesOfDice.addEventListener("keydown", (event)=>{
     if(event.key == 'Control' || event.key == 'C' || event.key == 'c' || event.key == 'V' || event.key == 'v'){
         setTimeout(() => {
             sidesOfDice.value = ""
-
             }, 100);
     }})
 */
@@ -60,8 +87,20 @@ function addToCurrentDice(){
 
 function checkInputType(input){
     if(isNaN(input)){
-        numbersToRoll.value = ""
+        numbersToRoll.value = "1";
+        diceToRoll = "1";
+        return false;
     }
+    return true;
+}
+
+function checkModifierType(input){
+    if(isNaN(input)){
+        modifierInput.value = "0";
+        modifierValue = "0";
+        return false;
+    }
+    return true;
 }
 
 let dicesSelector = document.querySelectorAll('.diceElement');
@@ -75,4 +114,15 @@ function changeDice(dice){
     dice.style.border = "1px solid gray"
     dice.style.borderRadius = "50%"
     
+}
+
+
+function operatorMod(element, operator){
+    
+    switch(operator){
+        case '+':
+            break;
+        case '-':
+            break;
+    }
 }
